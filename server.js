695c3819149fd0eb46a87279f17814f57cf4ea81
opedro-monteiro <pedro.oliveira@monteirodev.com>
2025-06-01 22:52:15 -0300

    Add CORS support and update package dependencies

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const { PORT } = require('./config.js');
-
+var cors = require('cors')
 let app = express();
+
+app.use(cors({
+  origin: '*',
+}))
+
 app.use(express.static('wwwroot'));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/models.js'));
